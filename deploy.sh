471b3b37436da46966eaace935ff1bddc2512c25
Jess Frazelle <acidburn@microsoft.com>
2018-11-20 16:15:59 -0500

    script
    Signed-off-by: Jess Frazelle <acidburn@microsoft.com>

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,9 @@
 set -e
 set -o pipefail
 
+if [[ "$GITHUB_REF" != "refs/heads/master" ]]; then
+	echo "$GITHUB_REF was not master, exiting..."
+	exit 0
+fi
+
 make aws-apply TERRAFORM_FLAGS=-auto-approve
